be06b54cf24c822a566910c3b35de8e44e793b19
Ashrf Mathkour Obeidat <ashrfmathkour@gmail.com>
2021-12-12 16:22:07 +0200

    feat:complain &explor

---
+++ pages/explore.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Header from '../components/header'
+import Post from '../components/Post'
+
+export default function explore() {
+    return (
+        <div>
+
+        <Header/> 
+       
+        <main className=' bg-slate-300'>
+            <div className='flex flex-col w-3/5 gap-3 m-auto'>
+            <Post/>  
+            <Post/>  
+            <Post/> 
+            </div>
+          
+
+
+        </main>
+        </div>
+
+    )
+}
