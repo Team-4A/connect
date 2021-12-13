097bc5685d3472c87fa2261d4af24443c466f3db
aseelalsaqer <aseel_fawwaz@yahoo.com>
2021-12-13 17:16:58 +0200

    chaning the card

---
+++ components/UserAcivity.js
@@ -0,0 +1,54 @@
+export default function Useractivity(){
+    return (
+        <div className="container w-full mx-auto ">
+        <div>
+
+            <div className="relative w-full mx-auto bg-white shadow-xl">
+               
+                
+                <div className="">
+                  
+                   
+
+                    <div className="w-full">
+                        <h3 className="px-4 font-bold text-center text-gray-600">Recent activites</h3>
+                        <div className="w-full mt-5">
+                            <a href="#" className="block w-full px-4 py-4 font-medium text-gray-600 transition duration-150 border-t-2 border-gray-100 hover:bg-gray-100">
+                                <img src="https://pantazisoft.com/img/avatar-2.jpeg" alt="" className="inline-block h-6 mr-2 rounded-full shadow-md"/>
+                                    Updated his status
+                                    <span className="text-sm text-gray-400">24 min ago</span>
+                            </a>
+
+                            <a href="#" className="block w-full px-4 py-4 font-medium text-gray-600 transition duration-150 border-t-2 border-gray-100 hover:bg-gray-100">
+                                <img src="https://pantazisoft.com/img/avatar-2.jpeg" alt="" className="inline-block h-6 mr-2 rounded-full shadow-md"/>
+                                    Added new profile picture
+                                    <span className="text-sm text-gray-400">42 min ago</span>
+                            </a>
+
+                            <a href="#" className="block w-full px-4 py-4 font-medium text-gray-600 transition duration-150 border-t-2 border-gray-100 hover:bg-gray-100">
+                                <img src="https://pantazisoft.com/img/avatar-2.jpeg" alt="" className="inline-block h-6 mr-2 rounded-full shadow-md"/>
+                                Posted new article in <span className="font-bold">Web Dev</span>
+                                <span className="text-sm text-gray-400">49 min ago</span>
+                            </a>
+
+                            <a href="#" className="block w-full px-4 py-4 font-medium text-gray-600 transition duration-150 border-t-2 border-gray-100 hover:bg-gray-100">
+                                <img src="https://pantazisoft.com/img/avatar-2.jpeg" alt="" className="inline-block h-6 mr-2 rounded-full shadow-md"/>
+                                Edited website settings
+                                <span className="text-sm text-gray-400">1 day ago</span>
+                            </a>
+
+                            <a href="#" className="block w-full px-4 py-4 font-medium text-gray-600 transition duration-150 border-t-2 border-gray-100 hover:bg-gray-100">
+                                <img src="https://pantazisoft.com/img/avatar-2.jpeg" alt="" className="inline-block h-6 mr-2 rounded-full shadow-md"/>
+                                Added new rank
+                                <span className="text-sm text-gray-400">5 days ago</span>
+                            </a>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import '../styles/mainScreen.css'
 import '../styles/userprofile.css'
+import '../styles/ourteam.css'
 import '../styles/companyprofile.css';
 import 'bootstrap/dist/css/bootstrap.css'; 
 function MyApp({ Component, pageProps }) {
