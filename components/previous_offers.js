d6d9f46d3577137ebfc46f34dad36ddbfda66a77
aseelalsaqer <aseel_fawwaz@yahoo.com>
2021-12-13 18:12:19 +0200

    user profile

--- components/previous_offers.js
+++ components/previous_offers.js
@@ -1,4 +1,4 @@
-export default function Useractivity(){
+export default function PreviousOffers(){
     return (
         <div className="container w-full mx-auto ">
         <div>
@@ -11,7 +11,7 @@ export default function Useractivity(){
                    
 
                     <div className="w-full">
-                        <h3 className="px-4 font-bold text-center text-gray-600">Recent activites</h3>
+                        <h3 className="px-4 font-bold text-center text-gray-600">Previous Offers</h3>
                         <div className="w-full mt-5">
                             <a href="#" className="block w-full px-4 py-4 font-medium text-gray-600 transition duration-150 border-t-2 border-gray-100 hover:bg-gray-100">
                                 <img src="https://pantazisoft.com/img/avatar-2.jpeg" alt="" className="inline-block h-6 mr-2 rounded-full shadow-md"/>
--- components/user_card.js
+++ components/user_card.js
@@ -1,123 +1,124 @@
 import Useractivity from '../components/user_activity'
+import PreviousOffers from '../components/previous_offers'
 // import PreviousOffers from '../components/previous_offers'
 export default function CompanyCard() {
    return (
      <>
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet"/>
-<div class="container">
-  <div class="row py-5 px-4">
-     <div class="">
-        <div id="content" class=" shadow rounded overflow-hidden">
+<div className="container">
+  <div className="px-4 py-5 row">
+     <div className="">
+        <div id="content" className="overflow-hidden rounded shadow ">
            {/* <!-- begin profile --> */}
-           <div class="profile ">
-              <div class="profile-header">
+           <div className="profile ">
+              <div className="profile-header">
                  {/* <!-- BEGIN profile-header-cover --> */}
-                 <div class="profile-header-cover"></div>
+                 <div className="profile-header-cover"></div>
                  {/* <!-- END profile-header-cover --> */}
                  {/* <!-- BEGIN profile-header-content --> */}
-                 <div class="profile-header-content">
+                 <div className="profile-header-content">
                     {/* <!-- BEGIN profile-header-img --> */}
-                    <div class="profile-header-img">
+                    <div className="profile-header-img">
                        <img src="https://bootdey.com/img/Content/avatar/avatar3.png" alt=""/>
                     </div>
                     {/* <!-- END profile-header-img -->
                     <!-- BEGIN profile-header-info --> */}
-                    <div class="profile-header-info">
-                       <h4 class="m-t-10 m-b-5">Sean Ngu</h4>
-                       <p class="m-b-10">UXUI + Frontend Developer</p>
-                       <a href="#" class="btn btn-sm btn-info mb-2">Edit Profile</a>
+                    <div className="profile-header-info">
+                       <h4 className="m-t-10 m-b-5">Sean Ngu</h4>
+                       <p className="m-b-10">UXUI + Frontend Developer</p>
+                       <a href="#" className="mb-2 btn btn-sm btn-info">Edit Profile</a>
                     </div>
                     {/* <!-- END profile-header-info --> */}
                  </div>
                  {/* <!-- END profile-header-content -->
                  <!-- BEGIN profile-header-tab --> */}
-                 <ul class="profile-header-tab nav nav-tabs">
-                    <li class="nav-item"><a href="#profile-post" class="nav-link active show" data-toggle="tab">POSTS</a></li>
-                    <li class="nav-item"><a href="#profile-about" class="nav-link" data-toggle="tab">ABOUT</a></li>
-                    {/* <li class="nav-item"><a href="#profile-photos" class="nav-link" data-toggle="tab">PHOTOS</a></li> */}
-                    {/* <li class="nav-item"><a href="#profile-videos" class="nav-link" data-toggle="tab">VIDEOS</a></li> */}
-                    <li class="nav-item"><a href="#profile-friends" class="nav-link" data-toggle="tab">FRIENDS</a></li>
+                 <ul className="profile-header-tab nav nav-tabs">
+                    <li className="nav-item"><a href="#profile-post" className="nav-link active show" data-toggle="tab">POSTS</a></li>
+                    <li className="nav-item"><a href="#profile-about" className="nav-link" data-toggle="tab">ABOUT</a></li>
+                    {/* <li className="nav-item"><a href="#profile-photos" className="nav-link" data-toggle="tab">PHOTOS</a></li> */}
+                    {/* <li className="nav-item"><a href="#profile-videos" className="nav-link" data-toggle="tab">VIDEOS</a></li> */}
+                    <li className="nav-item"><a href="#profile-friends" className="nav-link" data-toggle="tab">FRIENDS</a></li>
                  </ul>
                  {/* <!-- END profile-header-tab --> */}
               </div>
            </div>
            {/* <!-- end profile -->
            <!-- begin profile-content --> */}
-           <div class="row">
-           <div class="col-sm-6">
-             <div class="panel panel-white border-top-purple">
-               <div class="panel-heading">
-                 <h3 class="panel-title">My Story</h3>
-                 <div class="controls pull-right">
-                   <span class="pull-right clickable">
-                     <i class="fa fa-chevron-up"></i>
+           <div className="row">
+           <div className="col-sm-6">
+             <div className="panel panel-white border-top-purple">
+               <div className="panel-heading">
+                 <h3 className="panel-title">My Story</h3>
+                 <div className="controls pull-right">
+                   <span className="pull-right clickable">
+                     <i className="fa fa-chevron-up"></i>
                    </span>
                  </div>
                </div>
-               <div class="panel-body">
-                 <div class="body-section">
-                   <h5 class="section-heading">About</h5>
-                   <p class="section-content">A brief description of you</p>
+               <div className="panel-body">
+                 <div className="body-section">
+                   <h5 className="section-heading">About</h5>
+                   <p className="section-content">A brief description of you</p>
                  </div>
-                 <div class="body-section">
-                   <h5 class="section-heading">Introduction</h5>
-                   <p class="section-content">
-                     Put a little about yourself here so people know they've
+                 <div className="body-section">
+                   <h5 className="section-heading">Introduction</h5>
+                   <p className="section-content">
+                     Put a little about yourself here so people know they have
                      found the correct Kevin.
                    </p>
                  </div>
-                 <div class="body-section">
-                   <h5 class="section-heading">Acheivements</h5>
-                   <p class="section-content">
+                 <div className="body-section">
+                   <h5 className="section-heading">Acheivements</h5>
+                   <p className="section-content">
                      Examples: survived high school, have 3 kids, etc.
                    </p>
                  </div>
-                 <div class="body-section">
-                   <a href="#" class="btn btn-purple btn-sm">
+                 <div className="body-section">
+                   <a href="#" className="btn btn-purple btn-sm">
                      Edit
                    </a>
                  </div>
                </div>
              </div>
 
-             <div class="panel panel-white border-top-light-blue">
-               <div class="panel-heading">
-                 {/* <h3 class="panel-title">My Activities</h3> */}
-                 <div class="controls pull-right">
-                   <span class="pull-right clickable">
-                     <i class="fa fa-chevron-up"></i>
+             <div className="panel panel-white border-top-light-blue">
+               <div className="panel-heading">
+                 {/* <h3 className="panel-title">My Activities</h3> */}
+                 <div className="controls pull-right">
+                   <span className="pull-right clickable">
+                     <i className="fa fa-chevron-up"></i>
                    </span>
                  </div>
                </div>
-               <div class="panel-body">
-                 <div class="body-section">
-                   {/* <h5 class="section-heading">Current Plan: Basic Plan</h5> */}
+               <div className="panel-body">
+                 <div className="body-section">
+                   {/* <h5 className="section-heading">Current Plan: Basic Plan</h5> */}
                    <p><Useractivity/></p>
                    
                  </div>
-                 <div class="body-section">
-                   {/* <a href="#" class="btn btn-info btn-sm"> */}
+                 <div className="body-section">
+                   {/* <a href="#" className="btn btn-info btn-sm"> */}
                      {/* Upgrade Account */}
                    {/* </a> */}
                  </div>
                </div>
              </div>
 
-             <div class="panel panel-white border-top-pink">
-               <div class="panel-heading">
-                 <h3 class="panel-title">My Tasks</h3>
-                 <div class="controls pull-right">
-                   <span class="pull-right clickable">
-                     <i class="fa fa-chevron-up"></i>
+             <div className="panel panel-white border-top-pink">
+               <div className="panel-heading">
+                 <h3 className="panel-title">My Tasks</h3>
+                 <div className="controls pull-right">
+                   <span className="pull-right clickable">
+                     <i className="fa fa-chevron-up"></i>
                    </span>
                  </div>
                </div>
-               <div class="panel-body">
-                 <div class="body-section">
-                   <h5 class="mg-top-0">Completed Pre-Orders - 90%</h5>
-                   <div class="progress progress-xs">
+               <div className="panel-body">
+                 <div className="body-section">
+                   <h5 className="mg-top-0">Completed Pre-Orders - 90%</h5>
+                   <div className="progress progress-xs">
                      <div
-                       class="progress-bar"
+                       className="progress-bar"
                        role="progressbar"
                        aria-valuenow="90"
                        aria-valuemin="0"
@@ -126,11 +127,11 @@ export default function CompanyCard() {
                      ></div>
                    </div>
                  </div>
-                 <div class="body-section">
+                 <div className="body-section">
                    <h5>Completed Orders - 40%</h5>
-                   <div class="progress progress-xs">
+                   <div className="progress progress-xs">
                      <div
-                       class="progress-bar progress-bar-success"
+                       className="progress-bar progress-bar-success"
                        role="progressbar"
                        aria-valuenow="40"
                        aria-valuemin="0"
@@ -139,11 +140,11 @@ export default function CompanyCard() {
                      ></div>
                    </div>
                  </div>
-                 <div class="body-section">
+                 <div className="body-section">
                    <h5>Design New Ad - 20%</h5>
-                   <div class="progress progress-xs">
+                   <div className="progress progress-xs">
                      <div
-                       class="progress-bar progress-bar-info"
+                       className="progress-bar progress-bar-info"
                        role="progressbar"
                        aria-valuenow="20"
                        aria-valuemin="0"
@@ -152,26 +153,26 @@ export default function CompanyCard() {
                      ></div>
                    </div>
                  </div>
-                 <div class="body-section">
+                 <div className="body-section">
                    <h5>Conduct Feasibility Test - 60%</h5>
-                   <div class="progress progress-xs">
+                   <div className="progress progress-xs">
                      <div
-                       class="progress-bar progress-bar-warning"
+                       className="progress-bar progress-bar-warning"
                        role="progressbar"
                        aria-valuenow="60"
                        aria-valuemin="0"
                        aria-valuemax="100"
                        style={{ width: "60%" }}
                      >
-                       <span class="sr-only">60% Complete (warning)</span>
+                       <span className="sr-only">60% Complete (warning)</span>
                      </div>
                    </div>
                  </div>
-                 <div class="body-section">
+                 <div className="body-section">
                    <h5>Conduct Domain Analysis - 80%</h5>
-                   <div class="progress progress-xs">
+                   <div className="progress progress-xs">
                      <div
-                       class="progress-bar progress-bar-danger"
+                       className="progress-bar progress-bar-danger"
                        role="progressbar"
                        aria-valuenow="80"
                        aria-valuemin="0"
@@ -184,81 +185,83 @@ export default function CompanyCard() {
              </div>
            </div>
 
-           <div class="col-sm-6">
-             <div class="panel panel-white border-top-green">
-               <div class="panel-heading">
-                 <h3 class="panel-title">User Info</h3>
-                 <div class="controls pull-right">
-                   <span class="pull-right clickable">
-                     <i class="fa fa-chevron-up"></i>
+           <div className="col-sm-6">
+             <div className="panel panel-white border-top-green">
+               <div className="panel-heading">
+                 <h3 className="panel-title">User Info</h3>
+                 <div className="controls pull-right">
+                   <span className="pull-right clickable">
+                     <i className="fa fa-chevron-up"></i>
                    </span>
                  </div>
                </div>
-               <div class="panel-body">
-                 <div class="body-section">
-                   <h5 class="section-heading">Name</h5>
-                   <p class="section-content">Jane A. Doe</p>
+               <div className="panel-body">
+                 <div className="body-section">
+                   <h5 className="section-heading">Name</h5>
+                   <p className="section-content">Jane A. Doe</p>
                  </div>
-                 <div class="body-section">
-                   <h5 class="section-heading">Address</h5>
-                   <p class="section-content">
-                     46 Gray's Inn Rd, London, WC1X 8LP
+                 <div className="body-section">
+                   <h5 className="section-heading">Address</h5>
+                   <p className="section-content">
+                     46 Gray is Inn Rd, London, WC1X 8LP
                    </p>
                  </div>
-                 <div class="body-section">
-                   <h5 class="section-heading">Telephone</h5>
-                   <p class="section-content">(123) 456 - 7890</p>
+                 <div className="body-section">
+                   <h5 className="section-heading">Telephone</h5>
+                   <p className="section-content">(123) 456 - 7890</p>
                  </div>
-                 <div class="body-section">
-                   <h5 class="section-heading">Email</h5>
-                   <p class="section-content">spam@ztapps.com</p>
+                 <div className="body-section">
+                   <h5 className="section-heading">Email</h5>
+                   <p className="section-content">spam@ztapps.com</p>
                  </div>
-                 <div class="body-section">
-                   <h5 class="section-heading">Website</h5>
-                   <p class="section-content">ztapps.com</p>
+                 <div className="body-section">
+                   <h5 className="section-heading">Website</h5>
+                   <p className="section-content">ztapps.com</p>
                  </div>
-                 <div class="body-section">
-                   <a href="#" class="btn btn-green btn-sm">
+                 <div className="body-section">
+                   <a href="#" className="btn btn-green btn-sm">
                      Edit
                    </a>
                  </div>
                </div>
              </div>
 
-             {/* <div class="panel panel-white border-top-orange">
-               <div class="panel-heading">
-                 <h3 class="panel-title">Social Statistics</h3>
-                 <div class="controls pull-right">
-                   <span class="pull-right clickable">
-                     <i class="fa fa-chevron-up"></i>
+             {/* <div className="panel panel-white border-top-orange">
+               <div className="panel-heading">
+                 <h3 className="panel-title">Social Statistics</h3>
+                 <div className="controls pull-right">
+                   <span className="pull-right clickable">
+                     <i className="fa fa-chevron-up"></i>
                    </span>
                  </div>
                </div>
-               <div class="panel-body">
-                 <div class="body-section">
-                   <h5 class="section-heading">Friends</h5>
-                   <p class="section-content">242</p>
+               <div className="panel-body">
+                 <div className="body-section">
+                   <h5 className="section-heading">Friends</h5>
+                   <p className="section-content">242</p>
                  </div>
-                 <div class="body-section">
-                   <h5 class="section-heading">Posts</h5>
-                   <p class="section-content">2240</p>
+                 <div className="body-section">
+                   <h5 className="section-heading">Posts</h5>
+                   <p className="section-content">2240</p>
                  </div>
-                 <div class="body-section">
-                   <h5 class="section-heading">Pictures</h5>
-                   <p class="section-content">18</p>
+                 <div className="body-section">
+                   <h5 className="section-heading">Pictures</h5>
+                   <p className="section-content">18</p>
                  </div>
                </div>
              </div> */}
 
-             <div class="panel panel-white border-top-blue">
-               <div class="panel-heading">
-                 <h3 class="panel-title"></h3>
+             <div className="panel panel-white border-top-blue">
+               <div className="panel-heading">
+                 <h3 className="panel-title"></h3>
                  
+                  <p>
+                    <PreviousOffers/>
+                  </p>
 
-
-                 <div class="controls pull-right">
-                   <span class="pull-right clickable">
-                     <i class="fa fa-chevron-up"></i>
+                 <div className="controls pull-right">
+                   <span className="pull-right clickable">
+                     <i className="fa fa-chevron-up"></i>
                    </span>
                  </div>
                </div>
